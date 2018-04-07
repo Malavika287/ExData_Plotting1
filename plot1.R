959656132e6ac8a35e30c82cb4ca116bed309f96
Malavika <malavika.287@gmail.com>
2018-04-07 13:41:34 +0530

    Remove the header instruction while reading a file

--- plot1.R
+++ plot1.R
@@ -8,7 +8,7 @@
 
 
 #Read the data into a data frame skipping to the row pertaining to record for 02-01-2007
-df <- read.table("household_power_consumption.txt",header=TRUE,sep = ";",skip = 66637, nrow = 2880)
+df <- read.table("household_power_consumption.txt",sep = ";",skip = 66637, nrow = 2880)
 
 #Initialise the column names of the data frame
 names(df) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
